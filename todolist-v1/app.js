b6cb3b1c9ef2814c6306e88297cdde924150e8a8
p-tiku-b <59796125+p-tiku-b@users.noreply.github.com>
2020-06-16 18:07:08 +0530

    todoList files added

---
+++ todolist-v1/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["LEARN", "TO", "CODE"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  const day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+  const item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server running at port 3000.");
+});
